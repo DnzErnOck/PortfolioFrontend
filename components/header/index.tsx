1aea612052fd81bf9c4fabd628e0d608fec229e4
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2024-12-05 14:37:29 +0300

    base project setup

---
+++ components/header/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+import { VscCodeReview } from 'react-icons/vsc'
+import styles from "./styles.module.css";
+function Header() {
+  return (
+    <header className={`${styles.header} container fluid`}>
+      <div className={styles.headerWrapper}>
+        <Link className={styles.logo} href="/">
+            <VscCodeReview />
+        </Link> 
+        <nav className={styles.navigationMenu}>
+          <Link href='/'>Home</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Projects</Link>
+          <Link href='/'>Experience</Link>
+          <Link href='/'>Certificate</Link>
+          <Link href='/'>Contact</Link>
+          <Link href='/'>Blog</Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
