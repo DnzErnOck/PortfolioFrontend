6a9d5bf8f96e17939e268daf8530d5b7bd38ee6b
DnzErnOck <deniz.krtgl@gmail.com>
2025-05-13 16:03:45 +0300

    changed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+import path from 'path';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);  // Alias'ı ekliyoruz
+    config.resolve.alias['@'] = path.resolve(__dirname); // Alias'ı ekliyoruz
     return config;
   },
 };
