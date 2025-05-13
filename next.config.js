b5dec6a51c90d7c1683e01d509e7825ae052542c
DnzErnOck <deniz.krtgl@gmail.com>
2025-05-13 16:00:43 +0300

    config folder changed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-    reactStrictMode: true,
-    trailingSlash: true,
+  reactStrictMode: true,
+  trailingSlash: true,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);  // Alias'ı ekliyoruz
+    return config;
+  },
 };
 
 export default nextConfig;
