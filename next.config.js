5bebb4e9907fd8eca241395a8a2d75d9dcce3681
DnzErnOck <deniz.krtgl@gmail.com>
2025-05-15 10:59:52 +0300

    config changed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ import path from 'path';
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Bu satır ile env değişkenini ekliyoruz
+  },
   webpack(config) {
     // __dirname yerine, import.meta.url ile dizin yolunu alıyoruz
     const directory = path.dirname(new URL(import.meta.url).pathname);
@@ -10,5 +13,6 @@ const nextConfig = {
     return config;
   },
 };
+console.log("NEXT_PUBLIC_API_URL in browser:", process.env.NEXT_PUBLIC_API_URL);
 
 export default nextConfig;
